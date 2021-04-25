974648dac3c6c6a3ff3dc51c21c094631bb455ab
Aditya Kumar <adikumar@cs.washington.edu>
2021-04-25 13:58:25 -0700

    refactor characters to be read only and a little cleaner

--- src/level.ts
+++ src/level.ts
@@ -74,8 +74,6 @@ export class Level extends Scene {
   private tilesheet: SpriteSheet;
 
   private engine: Engine;
-
-  private characters: Character[];
   private enemies: Character[] = [];
   private players: Character[] = [];
   private terrain_data: CellType[][];
@@ -125,12 +123,16 @@ export class Level extends Scene {
     });
 
     this.tilemap.registerSpriteSheet("tile", this.tilesheet);
-    this.characters = spawnPoints.map(this.spawnCharacter);
+    spawnPoints.forEach(this.spawnCharacter);
     this.engine = engine;
 
     this.path_finder = new AStarFinder();
 
     this.playerTurn = true;
+  }
+
+  get characters() {
+    return [...this.enemies, ...this.players];
   }
 
   onInitialize(engine: Engine) {
@@ -392,9 +394,6 @@ export class Level extends Scene {
       const victimPos = this.pixelToTileCoords(victim.pos);
       this.players = this.players.filter((player) => player.id != victim.id);
       this.enemies = this.enemies.filter((player) => player.id != victim.id);
-      this.characters = this.characters.filter(
-        (player) => player.id != victim.id
-      );
 
       this.map_data[victimPos.x][victimPos.y].character = undefined;
     }
